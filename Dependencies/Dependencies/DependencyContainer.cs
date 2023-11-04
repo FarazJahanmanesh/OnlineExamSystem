a183d510aa208438c8de2f36e84234c5f1279a03
faraz jahan manesh <farazebipax@gmail.com>
2023-11-04 11:49:24 -0700

    add some files to project

---
+++ Dependencies/Dependencies/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependencies.Dependencies
+{
+    public static class DependencyContainer
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            #region add Dependencies
+            #endregion
+        }
+    }
+}
