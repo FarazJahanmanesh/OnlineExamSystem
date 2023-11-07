d13ed3b96fdcfd44498d10756f2b5850e1027660
faraz jahan manesh <farazebipax@gmail.com>
2023-11-07 09:05:30 -0800

    add user service with interface

---
+++ Application/Services/UserServices.cs
@@ -0,0 +1,53 @@
+﻿using Domain.Contracts.Repository;
+using Domain.Contracts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class UserServices : IUserServices
+    {
+        private readonly IUserRepository repository;
+        public UserServices(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task ChangePassword()
+        {
+            await repository.ChangePassword();
+        }
+
+        public async Task CreateUser()
+        {
+            await repository.CreateUser();
+        }
+
+        public async Task DeleteUser()
+        {
+            await repository.DeleteUser();
+        }
+
+        public async Task GetListOfUser()
+        {
+            await repository.GetListOfUser();
+        }
+
+        public async Task GetUser()
+        {
+            await repository.GetUser();
+        }
+
+        public async Task UpdateUser()
+        {
+            await repository.UpdateUser();
+        }
+
+        public async Task UserExsist()
+        { 
+            await repository.UserExsist();
+        }
+    }
+}
---
+++ Domain/Contracts/Services/IUserServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.Services
+{
+    public interface IUserServices
+    {
+        public Task UserExsist();
+        public Task ChangePassword();
+        public Task UpdateUser();
+        public Task GetUser();
+        public Task GetListOfUser();
+        public Task CreateUser();
+        public Task DeleteUser();
+    }
+}
