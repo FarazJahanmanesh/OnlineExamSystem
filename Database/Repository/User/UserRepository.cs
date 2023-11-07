529eee56182e06e97a361fc5d3dff327dff86048
faraz jahan manesh <farazebipax@gmail.com>
2023-11-07 08:59:36 -0800

    add interface for user repo

--- Database/Repository/User/UserRepository.cs
+++ Database/Repository/User/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Contracts.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Database.Repository.User
 {
-    public class UserRepository
+    public class UserRepository: IUserRepository
     {
         private readonly SystemDbContext dbContext;
         public UserRepository(SystemDbContext dbContext)
---
+++ Domain/Contracts/Repository/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.Repository
+{
+    public interface IUserRepository
+    {
+        public Task UserExsist();
+        public Task ChangePassword();
+        public Task UpdateUser();
+        public Task GetUser();
+        public Task GetListOfUser();
+        public Task CreateUser();
+        public Task DeleteUser();
+    }
+}
