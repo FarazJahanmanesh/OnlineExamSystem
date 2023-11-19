c79ba1476644ad1c0998d60acbed2130366779e3
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2023-11-19 16:36:52 +0330

    complete a repository

--- Database/Repository/Question/QuestionRepository.cs
+++ Database/Repository/Question/QuestionRepository.cs
@@ -1,37 +1,62 @@
-﻿using System;
+﻿using Domain.Dtos.QuestionDtos;
+using Mapster;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XAct;
 
 namespace Database.Repository.Question
 {
-    public class QuestionRepository
+    public class QuestionRepository: IQuestionRepository
     {
         private readonly SystemDbContext dbContext;
         public QuestionRepository(SystemDbContext dbContext)
         {
             this.dbContext = dbContext;
         }
-        public async Task GetQuestion()
+        public async Task<GetQuestionsDetailDto> GetQuestion()
         {
-
+            return await dbContext.Questions.AsNoTracking()
+                .ProjectToType<GetQuestionsDetailDto>()
+                .FirstOrDefaultAsync();
         }
-        public async Task GetQuestions()
+        public async Task<List<GetQuestionsDetailDto>> GetQuestions()
         {
-
+            return await dbContext.Questions.AsNoTracking()
+                .ProjectToType<GetQuestionsDetailDto>()
+                .Skip(0)
+                .Take(5)
+                .ToListAsync();
         }
-        public async Task DeleteQuestion()
+        public async Task<bool> DeleteQuestion(DeleteQuestionDetailDto dto)
         {
-
+            var question = await dbContext.Questions.FirstOrDefaultAsync();
+            if (question != null)
+            {
+                question=dto.Adapt(question);
+                await SaveChanges();
+                return true;
+            }
+            return false;
         }
-        public async Task UpdateQuestion()
+        public async Task<bool> UpdateQuestion(UpdateQuestionDetailDto dto)
         {
-
+            var question = await dbContext.Questions.FirstOrDefaultAsync();
+            if (question != null)
+            {
+                question = dto.Adapt(question);
+                await SaveChanges();
+                return true;
+            }
+            return false;
         }
-        public async Task AddQuestion()
+        public async Task AddQuestion(AddQuestionDetailDto dto)
         {
-            await dbContext.Questions.AddAsync()
+            await dbContext.Questions.AddAsync(dto.Adapt<Domain.Entities.Question>());
+            await SaveChanges();
         }
         private async Task SaveChanges()
         {
