9976c1cd556285897fc2948825e0f4b58cefa1c5
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2023-12-11 17:55:11 +0330

    ReplacePersianDigits

--- Application/Helpers/General.cs
+++ Application/Helpers/General.cs
@@ -12,5 +12,30 @@ namespace Application.Helpers
             var connectionString = Configuration.GetSection(section).Value.ToString();
             return connectionString;
         }
+        public static string ReplacePersianDigits(string input)
+        {
+            string[] persianDigits = { "۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹" };
+            string[] englishDigits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+            for (int i = 0; i < persianDigits.Length; i++)
+            {
+                input = input.Replace(persianDigits[i], englishDigits[i]);
+            }
+
+            return input;
+        }
+        //public static string ReplacePersianDigits(this string input)
+        //{
+        //    return input.Replace("۱", "1")
+        //                .Replace("۲", "2")
+        //                .Replace("۳", "3")
+        //                .Replace("۴", "4")
+        //                .Replace("۵", "5")
+        //                .Replace("۶", "6")
+        //                .Replace("۷", "7")
+        //                .Replace("۸", "8")
+        //                .Replace("۹", "9")
+        //                .Replace("۰", "0");
+        //}
     }
 }
