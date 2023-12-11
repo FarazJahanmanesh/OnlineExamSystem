bcb0dc2564be9dc52e61b6bfa194bed586a7c668
FarazJahanmanesh <jahanmaneshfaraz@gmail.com>
2023-12-11 17:35:40 +0330

    add general class to project

---
+++ Application/Helpers/General.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Application.Helpers
+{
+    public static class General
+    {
+        public static IConfigurationRoot Configuration;
+        public static string GetConfigurationValue(string section)
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+            var connectionString = Configuration.GetSection(section).Value.ToString();
+            return connectionString;
+        }
+    }
+}
